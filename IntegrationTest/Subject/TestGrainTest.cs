c5a2d63c3ba756d89f5e27da491cb934b11e3928
Elliot Newnham <elliot.newnham@gmail.com>
2021-02-18 11:58:21 -0500

    Add test for cancelling a task that was queued up

--- IntegrationTest/Subject/TestGrainTest.cs
+++ IntegrationTest/Subject/TestGrainTest.cs
@@ -19,14 +19,38 @@ namespace IntegrationTest.Subject
         }
 
         [Fact]
-        public async Task TestLongOperationAsync()
+        public async Task TestCancel()
         {
+            GrainCancellationTokenSource cts = new GrainCancellationTokenSource();
             ITestGrain grain = _clusterClient.GetGrain<ITestGrain>(123);
 
-            GrainCancellationTokenSource cts = new GrainCancellationTokenSource();
             await cts.Cancel();
 
-            await Assert.ThrowsAsync<TimeoutException>(() => grain.LongOperation(cts.Token, TimeSpan.FromSeconds(5)));
+            await Assert.ThrowsAsync<TaskCanceledException>(() => grain.LongOperation(cts.Token, TimeSpan.FromSeconds(5)));
+        }
+
+        [Fact]
+        public async Task TestCancel_WhenQueued()
+        {
+            GrainCancellationTokenSource cts1 = new GrainCancellationTokenSource();
+            GrainCancellationTokenSource cts2 = new GrainCancellationTokenSource();
+            ITestGrain grain = _clusterClient.GetGrain<ITestGrain>(123);
+
+            // Start a running task. Don't cancel this one
+            var runningTask = Task.Run(() => grain.LongOperation(cts1.Token, TimeSpan.FromSeconds(5)));
+
+            // Let running task run seccond with backpressure
+            await Task.WhenAny(runningTask, Task.Delay(TimeSpan.FromMilliseconds(100)));
+
+            // Queue a task. This is the one to cancel
+            var queuedTask = Task.Run(() => grain.LongOperation(cts2.Token, TimeSpan.FromSeconds(5)));
+
+            // Let both tasks run seccond with backpressure
+            await Task.WhenAny(runningTask, queuedTask, Task.Delay(TimeSpan.FromMilliseconds(100)));
+
+            await cts2.Cancel();
+
+            await Task.WhenAny( runningTask, Assert.ThrowsAsync<TaskCanceledException>(() => queuedTask) );
         }
     }
 }
